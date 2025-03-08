905b76e6e1f859aad729d1ed16081c1794b59fdc
sam <skel4720@uni.sydney.edu.au>
2025-03-08 11:18:33 +1100

    added file reading, day 1 star 1 complete

--- 1/solution.c
+++ 1/solution.c
@@ -8,15 +8,13 @@ int main()
     int* l1 = get_ints_from_file("input1", &n1);
     int* l2 = get_ints_from_file("input2", &n2);
 
-    merge_sort(l1, n1 - 1);
-    merge_sort(l1, n2 - 1);
+    merge_sort(l1, 0, n1 - 1);
+    merge_sort(l2, 0, n2 - 1);
 
     int diff = 0;
     for (int i = 0; i < n1; i++)
         diff += l1[i] > l2[i] ? l1[i] - l2[i] : l2[i] - l1[i];
 
-    print_arr(l1, n1);
-    print_arr(l2, n2);
     free(l1);
     free(l2);
 
