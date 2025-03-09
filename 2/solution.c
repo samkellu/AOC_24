eeeddc1aa1ca524ff4885df54246b822e998693d
sam <skel4720@uni.sydney.edu.au>
2025-03-09 15:15:29 +1100

    day 2 part 2 done poorly

--- 2/solution.c
+++ 2/solution.c
@@ -2,73 +2,63 @@
 #include <stdlib.h>
 #include "../lib/common.h"
 
-int get_num_safe(struct int_list* input, int n, int skip_per_row)
+int is_safe(struct int_list input, int skip_idx)
 {
-    int num_safe = 0;
-    for (int i = 0; i < n; i++)
-    {
+
         int all_increasing = 1;
         int all_decreasing = 1;
         int all_difference = 1;
-        int skips_left = skip_per_row;
-        for (int j = 1; j < input[i].count; j++)
+        int start = skip_idx == 0 || skip_idx == 1 ? 2 : 1;
+        for (int j = start; j < input.count; j++)
         {
-            int cur = input[i].values[j];
-            int prev = input[i].values[j-1];
-            if (cur > prev && all_decreasing)
-            {
-                if (skips_left)
-                {
-                    skips_left--;
-                    continue;
-                }
+            if (j == skip_idx)
+                continue;
 
+            int cur = input.values[j];
+            int prev = input.values[j-1 == skip_idx ? j-2 : j-1];
+            if (cur > prev && all_decreasing)
                 all_decreasing = 0;
-            }
 
             if (cur < prev && all_increasing)
-            {
-                if (skips_left)
-                {
-                    skips_left--;
-                    continue;
-                }
-
                 all_increasing = 0;
-            }
 
             int diff = cur > prev ? cur - prev : prev - cur;
             if (diff < 1 || diff > 3 )
-            {
-                if (skips_left)
-                {
-                    skips_left--;
-                    continue;
-                }
-                
                 all_difference = 0;
-            }
 
             if (!all_difference || !(all_decreasing || all_increasing))
                 break;
         }
 
-        if (all_difference && (all_decreasing || all_increasing))
-            num_safe++;
-    }
-
-    return num_safe;
+    return all_difference && (all_decreasing || all_increasing);
 } 
 
 int main()
 {
     int n;
     struct int_list* input = get_int_mat_from_file("input", &n);
+    int num_safe = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (is_safe(input[i], -1))
+            num_safe++;
+    }
 
-    int num_safe = get_num_safe(input, n, 0);
     printf("Number of safe reports: %d\n", num_safe);
 
-    num_safe = get_num_safe(input, n, 1);
+    num_safe = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = -1; j < input[i].count; j++)
+        {
+            if (is_safe(input[i], j))
+            {
+                num_safe++;
+                break;
+            }
+        }
+    }
+
     printf("Number of safe reports with problem dampener: %d\n", num_safe);
 
     for (int i = 0; i < n; i++)
